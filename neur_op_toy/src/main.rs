d6ee278ed5968d05ef19612d787d24c2c7bdf132
AHA-HH <anees1504@gmail.com>
2025-11-13 19:07:18 +0000

    clean up files

--- neur_op_toy/src/main.rs
+++
@@ -1,8 +0,0 @@
-use burn::tensor::Tensor;
-
-fn main() {
-    // Simple example to verify installation:
-    let device = burn::tensor::backend::ndarray::NdArrayDevice::default();
-    let x = Tensor::<burn::tensor::backend::ndarray::NdArrayBackend<f32>, 1>::from_floats([1.0, 2.0, 3.0], &device);
-    println!("{:?}", x);
-}
