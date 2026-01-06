a6ac9d3d8673af4520635a6f5be672a756dc8956
AHA-HH <anees1504@gmail.com>
2026-01-06 19:46:39 +0000

    started recursion derivative

--- try_toy_example/src/spectral.rs
+++ try_toy_example/src/spectral.rs
@@ -4,35 +4,41 @@ use scirs2_fft::{dct, idct, DCTType};
 
 // Function to carry out the DCT of Type II in 1D, from values to coefficients
 pub fn dct_1d<B: Backend>(
-    input: &Tensor<B, 1>,
+    values: &Tensor<B, 1>,
     device: &B::Device,
 ) -> Tensor<B, 1> {
     // Convert tensor input to a vector for crate usage
-    let input_vec = input.to_data().to_vec::<f64>().unwrap();
+    let values_vec = values.to_data().to_vec::<f64>().unwrap();
 
     // Compute DCT of input vector
-    let dct_coeffs_vec = dct(&input_vec, Some(DCTType::Type2), Some("ortho")).expect("Operation failed");
+    let coefficients_vec = dct(&values_vec, Some(DCTType::Type2), Some("ortho")).expect("Operation failed");
 
     // Convert back to a tensor
-    Tensor::<B, 1>::from_data(&*dct_coeffs_vec, device)
+    Tensor::<B, 1>::from_data(&*coefficients_vec, device)
 }
 
 // Function to carry out the inverse DCT of Type II in 1D, from coefficients to values
 pub fn idct_1d<B: Backend>(
-    input: &Tensor<B, 1>,
-    device: & B::Device,
+    coefficients: &Tensor<B, 1>,
+    device: &B::Device,
 ) -> Tensor<B, 1> {
     // Convert tensor input to a vector for crate usage
-    let input_vec = input.to_data().to_vec::<f64>().unwrap();
+    let coefficients_vec = coefficients.to_data().to_vec::<f64>().unwrap();
 
     // Compute inverse DCT of input vector
-    let idct_coeffs_vec = idct(&input_vec, Some(DCTType::Type2), Some("ortho")).expect("Operation failed");
+    let values_vec = idct(&coefficients_vec, Some(DCTType::Type2), Some("ortho")).expect("Operation failed");
 
     // Convert back to a tensor
-    Tensor::<B, 1>::from_data(&*idct_coeffs_vec, device)
+    Tensor::<B, 1>::from_data(&*values_vec, device)
 }
 
-// Function to apply the spectral derivative
+// Function to carry out the recursion for the derivative of the Chebyshev series
+pub fn cheb_derivative_recursion<B: Backend>(
+    coefficients: &Tensor<B, 1>,
+    device: &B::Device,
+) -> Tensor<B, 1> {
+
+}
 
 #[cfg(test)]
 mod test {
@@ -83,7 +89,7 @@ mod test {
     fn test_cheb_points_fft() {
         let device = <B as Backend>::Device::default();
 
-        let n = 5;
+        let n = 20;
         let points = gen_cheb_points::<B>(&device, n);
         let points_vec = points.to_data().to_vec::<f64>().unwrap();
 
@@ -94,8 +100,52 @@ mod test {
         let recovered = recovered_tensor.to_data().to_vec::<f64>().unwrap();
 
         for (i, &val) in points_vec.iter().enumerate() {
-            assert!((val - recovered[i]).abs() < 1e-15);
+            assert!((val - recovered[i]).abs() < 1e-14);
         }
+    }
+
+    #[test]
+    fn test_derivative_recursion() {
+        let device = <B as Backend>::Device::default();
+
+        let n = 5;
+        let points = gen_cheb_points::<B>(&device, n);
+        
+        // Testing function sinx
+        let values = points.clone().sin();
+
+        // First derivative cosx
+        let expected = points.clone().cos();
+        let expected_deriv_1 = expected.to_data().to_vec::<f64>().unwrap();
+
+        // Second derivative -sinx
+        let expected_2 = values.clone().neg();
+        // let expected_2: Vec<f64> = values.iter().map(|&x| -x).collect();
+
+        // Compute coefficients
+        let coeffs = dct_1d(&values.clone(), &device);
+
+        // Calculate first derivative
+        let deriv_1 = cheb_derivative_recursion(&coeffs, &device);
+
+        // Compute values
+        let calculated = idct_1d(&deriv_1, &device);
+
+        let computed_deriv_1 = calculated.to_data().to_vec::<f64>().unwrap();
+
+        println!("Vector {:#?}", expected_deriv_1);
+
+        println!("Vector {:#?}", computed_deriv_1);
+
+        // Compare first derivative
+        for (i, &val) in expected_deriv_1.iter().enumerate() {
+            assert!((val - computed_deriv_1[i]).abs() < 1e-14);
+        }
+
+        // Compute coefficients
+
+    
+
     }
 
 }
